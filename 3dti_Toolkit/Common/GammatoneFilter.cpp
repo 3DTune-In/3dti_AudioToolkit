d1e7433a7fe85e3ae26469384018a706a4e49569
Michael Krzyzaniak <mkrzyzan@asu.edu>
2018-11-18 04:55:43 +0000

    Commandment 1: Thou shalt not change another programmer's tabs to spaces.
    Changed spaces back to tabs

--- 3dti_Toolkit/Common/GammatoneFilter.cpp
+++ 3dti_Toolkit/Common/GammatoneFilter.cpp
@@ -10,7 +10,7 @@
 * John Holdsworth, Ian Nimmo-Smith, Roy Patterson, Peter Rice
 * 26th February 1988
 *
-* \authors 3DI-DIANA Research Group (University of Malaga), in alphabetical order: M. Cuevas-Rodriguez, C. Garre,  D. Gonzalez-Toledo, E.J. de la Rubia-Cuestas, L. Molina-Tanco ||
+* \authors 3DI-DIANA Research Group (University of Malaga), in alphabetical order: M. Cuevas-Rodriguez, C. Garre, D. Gonzalez-Toledo, E.J. de la Rubia-Cuestas, L. Molina-Tanco ||
 * Coordinated by , A. Reyes-Lecuona (University of Malaga) and L.Picinali (Imperial College London) ||
 * \b Contact: areyes@uma.es and l.picinali@imperial.ac.uk
 *
@@ -54,130 +54,130 @@ namespace Common {
 	//////////////////////////////////////////////
 	CGammatoneFilter::CGammatoneFilter(unsigned _order, float _centerFrequency)
 	{
-    //the upper bound is limited by the factorial function in the calculation of an.
-    //an should be precomputed for, say 20 filter orders, and that should be the upper bound here
-    if ((_order < 1) || (_order > 7))
-    {
-      SET_RESULT(RESULT_ERROR_BADSIZE, "Gammatone filter needs to be constructed with a order greater than 0 (try 4 for modelling the basilar membrane)");
-      return;
-    }
-  
-    order = _order;
-    an = CalculateAn(order);
-    cn = CalculateCn(order);
-  
-    prev_z_real = new float[order]();
-    prev_z_imag = new float[order]();
-    prev_w_real = new float[order]();
-    prev_w_imag = new float[order]();
-    //todo: check that we got the memory without stack overflow?
+		//the upper bound is limited by the factorial function in the calculation of an.
+		//an should be precomputed for, say 20 filter orders, and that should be the upper bound here
+		if ((_order < 1) || (_order > 7))
+		{
+			SET_RESULT(RESULT_ERROR_BADSIZE, "Gammatone filter needs to be constructed with a order greater than 0 (try 4 for modelling the basilar membrane)");
+			return;
+		}
+	
+		order = _order;
+		an = CalculateAn(order);
+		cn = CalculateCn(order);
+	
+		prev_z_real = new float[order]();
+		prev_z_imag = new float[order]();
+		prev_w_real = new float[order]();
+		prev_w_imag = new float[order]();
+		//todo: check that we got the memory without stack overflow?
 
 		generalGain = 1.0f;
 
-    phase = 0;
-    // error handler: Trust in SetSamplingFreq for result
-    SetSamplingFreq(DEFAULT_SAMPLING_RATE);
-    SetFrequencyUsingERBOfHumanAuditoryFilter(_centerFrequency);
+		phase = 0;
+		// error handler: Trust in SetSamplingFreq for result
+		SetSamplingFreq(DEFAULT_SAMPLING_RATE);
+		SetFrequencyUsingERBOfHumanAuditoryFilter(_centerFrequency);
 	}
 
 	//////////////////////////////////////////////
 	//void CGammatoneFilter::Process(CMonoBuffer<float> &inBuffer, CMonoBuffer<float> & outBuffer, bool addResult)
 	//{
-  //  SET_RESULT(RESULT_ERROR_BADSIZE, "I haven't implemented this yet. Is it needed?");
+	//	SET_RESULT(RESULT_ERROR_BADSIZE, "I haven't implemented this yet. Is it needed?");
 	//}
 
-  //////////////////////////////////////////////
-  void CGammatoneFilter::Process(CMonoBuffer<float> &buffer)
-  {
-    int size = buffer.size();
+	//////////////////////////////////////////////
+	void CGammatoneFilter::Process(CMonoBuffer<float> &buffer)
+	{
+		int size = buffer.size();
 
-    if (size <= 0)
-    {
-      SET_RESULT(RESULT_ERROR_BADSIZE, "Attempt to process a biquad filter with an empty input buffer");
-      return;
-    }
+		if (size <= 0)
+		{
+			SET_RESULT(RESULT_ERROR_BADSIZE, "Attempt to process a biquad filter with an empty input buffer");
+			return;
+		}
 
-    //SET_RESULT(RESULT_OK, "Biquad filter process succesfull");
+		//SET_RESULT(RESULT_OK, "Biquad filter process succesfull");
 
-    //todo: make these instance variables and decleare them at filter creation time?
-    //ok, but what if the buffer size changes?
-    //CMonoBuffer<float> z_real(size);
-    //CMonoBuffer<float> z_imag(size);
-    float* z_real = new float[size]();
-    float* z_imag = new float[size]();
-    //todo: check that we got the memory without stack overflow?
-  
-    //todo: implement data-doubling to avoid ailising for high freqs > 11 kHz
-    double phase_increment = this->f0 * M_TWO_PI / this->samplingFreq;
-  
-    //constant 1-e^(-2PI*b*t), for middle parenthesis term of eq 10
-    //could be calculated when bandwidth or samplingFreq are calculated
-    double constant = 1.0 - DEFAULT_EXP_FUNCTION(-M_TWO_PI * this->b / this->samplingFreq);
-  
-    //extra storage for intermediate computations
-    float w_real, w_imag, sin_phase, cos_phase;
-  
-    for (int k = 0; k < size; k++)
-    {
-      cos_phase = DEFAULT_COS_FUNCTION(this->phase);
-      sin_phase = DEFAULT_SIN_FUNCTION(this->phase);
-      //eq. 9 frequency shifting by -f0 Hz
-      z_real[k] =  cos_phase * buffer[k];
-      z_imag[k] = -sin_phase * buffer[k];
+		//todo: make these instance variables and decleare them at filter creation time?
+		//ok, but what if the buffer size changes?
+		//CMonoBuffer<float> z_real(size);
+		//CMonoBuffer<float> z_imag(size);
+		float* z_real = new float[size]();
+		float* z_imag = new float[size]();
+		//todo: check that we got the memory without stack overflow?
+	
+		//todo: implement data-doubling to avoid ailising for high freqs > 11 kHz
+		double phase_increment = this->f0 * M_TWO_PI / this->samplingFreq;
+	
+		//constant 1-e^(-2PI*b*t), for middle parenthesis term of eq 10
+		//could be calculated when bandwidth or samplingFreq are calculated
+		double constant = 1.0 - DEFAULT_EXP_FUNCTION(-M_TWO_PI * this->b / this->samplingFreq);
+	
+		//extra storage for intermediate computations
+		float w_real, w_imag, sin_phase, cos_phase;
+	
+		for (int k = 0; k < size; k++)
+		{
+			cos_phase = DEFAULT_COS_FUNCTION(this->phase);
+			sin_phase = DEFAULT_SIN_FUNCTION(this->phase);
+			//eq. 9 frequency shifting by -f0 Hz
+			z_real[k] = cos_phase * buffer[k];
+			z_imag[k] = -sin_phase * buffer[k];
 
-      //eq. 10 recursive 1st order filter
-      for (int n = 0; n < this->order; n++)
-      {
-        //last parenthesis
-        w_real = this->prev_z_real[n] - this->prev_w_real[n];
-        w_imag = this->prev_z_imag[n] - this->prev_w_imag[n];
-      
-        //second term
-        w_real *= constant;
-        w_imag *= constant;
-      
-        //first term
-        w_real += this->prev_w_real[n];
-        w_imag += this->prev_w_imag[n];
-        
-        this->prev_z_real[n] = z_real[k];
-        this->prev_z_imag[n] = z_imag[k];
-        this->prev_w_real[n] = z_real[k] = w_real;
-        this->prev_w_imag[n] = z_imag[k] = w_imag;
-        z_real[k] = w_real;
-        z_imag[k] = w_imag;
-      }
-   
-      //equation 11 frequency shifting by +f0 Hz
-      buffer[k] = cos_phase*z_real[k] - sin_phase*z_imag[k];
-      buffer[k] *= this->generalGain;
-    
-      this->phase += phase_increment;
-      if(this->phase > M_TWO_PI)
-        this->phase -= M_TWO_PI;
-    }
-    //todo: this delayed the signal by half of the sampling period...
-  }
-  
-  //////////////////////////////////////////////
-  void CGammatoneFilter::SetSamplingFreq(float _samplingFreq)
-  {
-    if (_samplingFreq < 0.1)
-    {
-      SET_RESULT(RESULT_ERROR_INVALID_PARAM, "Sampling frequency for gammatone filter is invalid");
-      return;
-    }
+			//eq. 10 recursive 1st order filter
+			for (int n = 0; n < this->order; n++)
+			{
+				//last parenthesis
+				w_real = this->prev_z_real[n] - this->prev_w_real[n];
+				w_imag = this->prev_z_imag[n] - this->prev_w_imag[n];
+			
+				//second term
+				w_real *= constant;
+				w_imag *= constant;
+			
+				//first term
+				w_real += this->prev_w_real[n];
+				w_imag += this->prev_w_imag[n];
+				
+				this->prev_z_real[n] = z_real[k];
+				this->prev_z_imag[n] = z_imag[k];
+				this->prev_w_real[n] = z_real[k] = w_real;
+				this->prev_w_imag[n] = z_imag[k] = w_imag;
+				z_real[k] = w_real;
+				z_imag[k] = w_imag;
+			}
+	 
+			//equation 11 frequency shifting by +f0 Hz
+			buffer[k] = cos_phase*z_real[k] - sin_phase*z_imag[k];
+			buffer[k] *= this->generalGain;
+		
+			this->phase += phase_increment;
+			if(this->phase > M_TWO_PI)
+				this->phase -= M_TWO_PI;
+		}
+		//todo: this delayed the signal by half of the sampling period...
+	}
+	
+	//////////////////////////////////////////////
+	void CGammatoneFilter::SetSamplingFreq(float _samplingFreq)
+	{
+		if (_samplingFreq < 0.1)
+		{
+			SET_RESULT(RESULT_ERROR_INVALID_PARAM, "Sampling frequency for gammatone filter is invalid");
+			return;
+		}
 
-    SET_RESULT(RESULT_OK, "Sampling frequency for gammatone filter succesfully set");
-    samplingFreq = _samplingFreq;
-  }
+		SET_RESULT(RESULT_OK, "Sampling frequency for gammatone filter succesfully set");
+		samplingFreq = _samplingFreq;
+	}
 
-  //////////////////////////////////////////////
-  float CGammatoneFilter::GetSamplingFreq()
-  {
-    return samplingFreq;
-  }
-  
+	//////////////////////////////////////////////
+	float CGammatoneFilter::GetSamplingFreq()
+	{
+		return samplingFreq;
+	}
+	
 	//////////////////////////////////////////////
 	void CGammatoneFilter::SetGeneralGain(float _gain)
 	{
@@ -190,127 +190,127 @@ namespace Common {
 		return generalGain;
 	}
  
-  //////////////////////////////////////////////
-  unsigned CGammatoneFilter::GetOrder()
-  {
-    return order;
-  }
-  
-  //////////////////////////////////////////////
-  void CGammatoneFilter::Set3dBBandwidth(float _bw)
-  {
-    b = _bw / cn;
-  }
-  
-  //////////////////////////////////////////////
-  float CGammatoneFilter::Get3dBBandwidth()
-  {
-    return cn * b;
-  }
+	//////////////////////////////////////////////
+	unsigned CGammatoneFilter::GetOrder()
+	{
+		return order;
+	}
+	
+	//////////////////////////////////////////////
+	void CGammatoneFilter::Set3dBBandwidth(float _bw)
+	{
+		b = _bw / cn;
+	}
+	
+	//////////////////////////////////////////////
+	float CGammatoneFilter::Get3dBBandwidth()
+	{
+		return cn * b;
+	}
 
-  //////////////////////////////////////////////
-  void CGammatoneFilter::SetERBBandwidth(float _erb)
-  {
-    b = _erb / an;
-  }
-  
-  //////////////////////////////////////////////
-  float CGammatoneFilter::GetERBBandwidth()
-  {
-    return an * b;
-  }
+	//////////////////////////////////////////////
+	void CGammatoneFilter::SetERBBandwidth(float _erb)
+	{
+		b = _erb / an;
+	}
+	
+	//////////////////////////////////////////////
+	float CGammatoneFilter::GetERBBandwidth()
+	{
+		return an * b;
+	}
 
-  //////////////////////////////////////////////
-  void CGammatoneFilter::SetCenterFrequency(float _freq)
-  {
-    f0 = _freq;
-  }
-  
-  //////////////////////////////////////////////
-  float CGammatoneFilter::GetCenterFrequency()
-  {
-    return f0;
-  }
-  
-  //////////////////////////////////////////////
-  void CGammatoneFilter::SetFrequencyUsingERBOfHumanAuditoryFilter(float _freq)
-  {
-    float erb = GetERBOfHumanAuditoryFilter(_freq);
-    SetCenterFrequency(_freq);
-    SetERBBandwidth(erb);
-  }
-  
-  //////////////////////////////////////////////
-  float CGammatoneFilter::GetERBOfHumanAuditoryFilter(float _freq)
-  {
-    float result = 0;
-  
-    /* equation 7 */
-    /* this is supposedly valid from 100 to 10000 Hz. I don't know above 10000Hz */
-    /* NB, Wikipedia s.v. "ERB" give the wrong coefficients (missing order of magnitude terms) */
-    if(_freq > 100)
-      result = (6.23e-6 * _freq * _freq) + (93.39e-3 * _freq) + 28.52;
-  
-    /* this is supposedly valid for frequencies ~ 0.1 to 10 Hz. I don't know from 10 to 100 Hz */
-    //todo: check the coefficients against Moore and Glassberg 1983
-    //B.C.J. Moore and B.R. Glasberg, "Suggested formulae for calculating auditory-filter bandwidths and excitation patterns" Journal of the Acoustical Society of America 74: 750-753, 1983
-    else
-      result = 24.7 * (4.37*_freq+1);
-  
-    return result;
-  }
-  
-  //////////////////////////////////////////////
-  //constructor ensures that x (filter order) is 1 to 12
-  unsigned CGammatoneFilter::Factorial(unsigned x)
-  {
-    unsigned result = 1;
-  
-    while(x > 1)
-      result *= x--;
-  
-    return result;
-  }
-  
-  //////////////////////////////////////////////
-  double CGammatoneFilter::CalculateAn(unsigned _order)
-  {
-    if (_order < 1)
-    {
-      SET_RESULT(RESULT_ERROR_BADSIZE, "ERB of Gammatone filter needs an order greater than 0");
-      return 1;
-    }
-    SET_RESULT(RESULT_OK, "OrderToEquivalentRectangularBandwidth OK");
-  
-    //equation 6
-    //todo: precompute an using bignums, and here just look it up in a table and return it.
-    long double two_n_minus_two     = (2.0 * _order) - 2.0;
-    long double two_n_minus_two_WOW = DEFAULT_FACTORIAL_FUNCTION(two_n_minus_two);
-    long double two_to_the_neg_x    = DEFAULT_POW_FUNCTION(2.0, -two_n_minus_two);
-    long double numerator           = M_PI * two_n_minus_two_WOW * two_to_the_neg_x;
-    long double denominator         = DEFAULT_FACTORIAL_FUNCTION(_order-1);
-    denominator *= denominator;
-  
-    if(denominator == 0)
-    {
-      SET_RESULT(RESULT_ERROR_BADSIZE, "Numerical error while setting bandwidth of gammatone filter (is the filter order too high?)");
-      return 1.0;
-    }
-  
-    return numerator / denominator;
-  }
-  
-  //////////////////////////////////////////////
-  double CGammatoneFilter::CalculateCn(unsigned _order)
-  {
-    if (_order < 1)
-    {
-      SET_RESULT(RESULT_ERROR_BADSIZE, "ERB of Gammatone filter needs an order greater than 0");
-      return 1.0;
-    }
-    SET_RESULT(RESULT_OK, "OrderToEquivalentRectangularBandwidth OK");
-  
-    //equation 7
-    return 2.0 * DEFAULT_SQRT_FUNCTION(DEFAULT_POW_FUNCTION(2, 1.0/(double)_order) - 1.0);
-  }
+	//////////////////////////////////////////////
+	void CGammatoneFilter::SetCenterFrequency(float _freq)
+	{
+		f0 = _freq;
+	}
+	
+	//////////////////////////////////////////////
+	float CGammatoneFilter::GetCenterFrequency()
+	{
+		return f0;
+	}
+	
+	//////////////////////////////////////////////
+	void CGammatoneFilter::SetFrequencyUsingERBOfHumanAuditoryFilter(float _freq)
+	{
+		float erb = GetERBOfHumanAuditoryFilter(_freq);
+		SetCenterFrequency(_freq);
+		SetERBBandwidth(erb);
+	}
+	
+	//////////////////////////////////////////////
+	float CGammatoneFilter::GetERBOfHumanAuditoryFilter(float _freq)
+	{
+		float result = 0;
+	
+		/* equation 7 */
+		/* this is supposedly valid from 100 to 10000 Hz. I don't know above 10000Hz */
+		/* NB, Wikipedia s.v. "ERB" give the wrong coefficients (missing order of magnitude terms) */
+		if(_freq > 100)
+			result = (6.23e-6 * _freq * _freq) + (93.39e-3 * _freq) + 28.52;
+	
+		/* this is supposedly valid for frequencies ~ 0.1 to 10 Hz. I don't know from 10 to 100 Hz */
+		//todo: check the coefficients against Moore and Glassberg 1983
+		//B.C.J. Moore and B.R. Glasberg, "Suggested formulae for calculating auditory-filter bandwidths and excitation patterns" Journal of the Acoustical Society of America 74: 750-753, 1983
+		else
+			result = 24.7 * (4.37*_freq+1);
+	
+		return result;
+	}
+	
+	//////////////////////////////////////////////
+	//constructor ensures that x (filter order) is 1 to 12
+	unsigned CGammatoneFilter::Factorial(unsigned x)
+	{
+		unsigned result = 1;
+	
+		while(x > 1)
+			result *= x--;
+	
+		return result;
+	}
+	
+	//////////////////////////////////////////////
+	double CGammatoneFilter::CalculateAn(unsigned _order)
+	{
+		if (_order < 1)
+		{
+			SET_RESULT(RESULT_ERROR_BADSIZE, "ERB of Gammatone filter needs an order greater than 0");
+			return 1;
+		}
+		SET_RESULT(RESULT_OK, "OrderToEquivalentRectangularBandwidth OK");
+	
+		//equation 6
+		//todo: precompute an using bignums, and here just look it up in a table and return it.
+		long double two_n_minus_two	 = (2.0 * _order) - 2.0;
+		long double two_n_minus_two_WOW = DEFAULT_FACTORIAL_FUNCTION(two_n_minus_two);
+		long double two_to_the_neg_x		= DEFAULT_POW_FUNCTION(2.0, -two_n_minus_two);
+		long double numerator			= M_PI * two_n_minus_two_WOW * two_to_the_neg_x;
+		long double denominator		 = DEFAULT_FACTORIAL_FUNCTION(_order-1);
+		denominator *= denominator;
+	
+		if(denominator == 0)
+		{
+			SET_RESULT(RESULT_ERROR_BADSIZE, "Numerical error while setting bandwidth of gammatone filter (is the filter order too high?)");
+			return 1.0;
+		}
+	
+		return numerator / denominator;
+	}
+	
+	//////////////////////////////////////////////
+	double CGammatoneFilter::CalculateCn(unsigned _order)
+	{
+		if (_order < 1)
+		{
+			SET_RESULT(RESULT_ERROR_BADSIZE, "ERB of Gammatone filter needs an order greater than 0");
+			return 1.0;
+		}
+		SET_RESULT(RESULT_OK, "OrderToEquivalentRectangularBandwidth OK");
+	
+		//equation 7
+		return 2.0 * DEFAULT_SQRT_FUNCTION(DEFAULT_POW_FUNCTION(2, 1.0/(double)_order) - 1.0);
+	}
 }
--- 3dti_Toolkit/Common/GammatoneFilter.h
+++ 3dti_Toolkit/Common/GammatoneFilter.h
@@ -31,12 +31,12 @@ namespace Common {
 
 	/** \details This class implements a gammatone filter
 	\n using the algorithm described in this paper:
-  \n Implementing a GammaTone Filter Bank
-  \n Annex C of the SVOS Final Report (Part A: The Auditory Filter Bank)
-  \n John Holdsworth, Ian Nimmo-Smith, Roy Patterson, Peter Rice
-  \n 26th February 1988
-  \n  https://www.pdn.cam.ac.uk/other-pages/cnbh/files/publications/SVOSAnnexC1988.pdf
-  */
+	\n Implementing a GammaTone Filter Bank
+	\n Annex C of the SVOS Final Report (Part A: The Auditory Filter Bank)
+	\n John Holdsworth, Ian Nimmo-Smith, Roy Patterson, Peter Rice
+	\n 26th February 1988
+	\n  https://www.pdn.cam.ac.uk/other-pages/cnbh/files/publications/SVOSAnnexC1988.pdf
+	*/
 	class CGammatoneFilter
 	{
 	public:
@@ -46,7 +46,7 @@ namespace Common {
 
 		/** \brief Default constructor.
 		*	\details By default, sets sampling frequency to 44100Hz.
-    *  \param [in] _order -- the filter order. This cannot be changed after the filter is constructed. The typical value is 4.
+		*  \param [in] _order -- the filter order. This cannot be changed after the filter is constructed. The typical value is 4.
 		*   \eh On error, an error code is reported to the error handler.
 		*/
 		CGammatoneFilter(unsigned _order, float _centerFrequency);
@@ -65,19 +65,19 @@ namespace Common {
 		*/
 		void Process(CMonoBuffer<float> &buffer);
 
-    /** \brief Set the sampling frequency at which audio samples were acquired
-    *  \param [in] _samplingFreq sampling frequency, in Hertzs
-    *   \eh On success, RESULT_OK is reported to the error handler.
-    *       On error, an error code is reported to the error handler.
-    */
-    void SetSamplingFreq(float _samplingFreq);
+		/** \brief Set the sampling frequency at which audio samples were acquired
+		*  \param [in] _samplingFreq sampling frequency, in Hertzs
+		*   \eh On success, RESULT_OK is reported to the error handler.
+		*       On error, an error code is reported to the error handler.
+		*/
+		void SetSamplingFreq(float _samplingFreq);
 
-    /** \brief Get the sample rate, in Hz, of the filter
-    *  \retval freq filter sampling frequency
-    *   \eh Nothing is reported to the error handler.
-    */
-    float GetSamplingFreq();
-  
+		/** \brief Get the sample rate, in Hz, of the filter
+		*  \retval freq filter sampling frequency
+		*   \eh Nothing is reported to the error handler.
+		*/
+		float GetSamplingFreq();
+	
 		/** \brief Set the gain of the filter 
 		*	\param [in] _gain filter gain 
 		*   \eh Nothing is reported to the error handler.
@@ -90,60 +90,60 @@ namespace Common {
 		*/
 		float GetGeneralGain();
 
-    /** \brief Get the order of the filter
-    *  \retval order filter order
-    *   \eh Nothing is reported to the error handler.
-    */
-    unsigned GetOrder();
+		/** \brief Get the order of the filter
+		*  \retval order filter order
+		*   \eh Nothing is reported to the error handler.
+		*/
+		unsigned GetOrder();
 
-    /** \brief Set the bandwidth of the filter, specified as the width between the 3dB cutoff points, and retaining the current center frequency
-    *  \param [in] _bw 3dB bandwidth
-    *   \eh Nothing is reported to the error handler.
-    */
-    void Set3dBBandwidth(float _bw);
+		/** \brief Set the bandwidth of the filter, specified as the width between the 3dB cutoff points, and retaining the current center frequency
+		*  \param [in] _bw 3dB bandwidth
+		*   \eh Nothing is reported to the error handler.
+		*/
+		void Set3dBBandwidth(float _bw);
 
-    /** \brief Get the bandwidth of the filter, specified as the width between the 3dB cutoff points
-    *  \retval bw filter bandwidth (3db cutoff)
-    *   \eh Nothing is reported to the error handler.
-    */
-    float Get3dBBandwidth();
+		/** \brief Get the bandwidth of the filter, specified as the width between the 3dB cutoff points
+		*  \retval bw filter bandwidth (3db cutoff)
+		*   \eh Nothing is reported to the error handler.
+		*/
+		float Get3dBBandwidth();
 
-    /** \brief Set the bandwidth of the filter, specified as the equivalent rectangular bandwidth, and retaining the current center frequency
-    *  \param [in] _freq center frequency
-    *   \eh Nothing is reported to the error handler.
-    */
-    void SetERBBandwidth(float _erb);
+		/** \brief Set the bandwidth of the filter, specified as the equivalent rectangular bandwidth, and retaining the current center frequency
+		*  \param [in] _freq center frequency
+		*   \eh Nothing is reported to the error handler.
+		*/
+		void SetERBBandwidth(float _erb);
 
-    /** \brief Get the bandwidth of the filter, specified as the equivalent rectangular bandwidth,
-    *  \retval erb filter bandwidth (3db cutoff)
-    *   \eh Nothing is reported to the error handler.
-    */
-    float GetERBBandwidth();
-  
-    /** \brief Set the center frequency of the filter, retaining the current bandwidth
-    *  \param [in] _freq center frequency
-    *   \eh Nothing is reported to the error handler.
-    */
-    void SetCenterFrequency(float _freq);
+		/** \brief Get the bandwidth of the filter, specified as the equivalent rectangular bandwidth,
+		*  \retval erb filter bandwidth (3db cutoff)
+		*   \eh Nothing is reported to the error handler.
+		*/
+		float GetERBBandwidth();
+	
+		/** \brief Set the center frequency of the filter, retaining the current bandwidth
+		*  \param [in] _freq center frequency
+		*   \eh Nothing is reported to the error handler.
+		*/
+		void SetCenterFrequency(float _freq);
 
-    /** \brief Get the center frequency of the filter
-    *  \retval freq filter center freq
-    *   \eh Nothing is reported to the error handler.
-    */
-    float GetCenterFrequency();
-  
-    /** \brief Set the center frequency of the filter, and adjust the bandwidth to match the Equivalent Rectangular Bandwidth (ERB) of the human auditory filter for the given frequency. This is a convience method that calls SetCenterFrequency, GetERBOfHumanAudirotyFilter, and SetERBBandwidth.
-    *  \param [in] _freq center frequency
-    *   \eh Nothing is reported to the error handler.
-    */
-    void SetFrequencyUsingERBOfHumanAuditoryFilter(float _freq);
+		/** \brief Get the center frequency of the filter
+		*  \retval freq filter center freq
+		*   \eh Nothing is reported to the error handler.
+		*/
+		float GetCenterFrequency();
+	
+		/** \brief Set the center frequency of the filter, and adjust the bandwidth to match the Equivalent Rectangular Bandwidth (ERB) of the human auditory filter for the given frequency. This is a convience method that calls SetCenterFrequency, GetERBOfHumanAudirotyFilter, and SetERBBandwidth.
+		*  \param [in] _freq center frequency
+		*   \eh Nothing is reported to the error handler.
+		*/
+		void SetFrequencyUsingERBOfHumanAuditoryFilter(float _freq);
 
-    /** \brief Helper function (class method) to calculate the ERB of the human auditory filter for a given frequecy
-    *  \param [in] _freq center frequency
-    *  \retval erb equivalent rectangular bandwidth (ERB) for the given center frequency
-    *   \eh Nothing is reported to the error handler.
-    */
-    static float GetERBOfHumanAuditoryFilter(float _freq);
+		/** \brief Helper function (class method) to calculate the ERB of the human auditory filter for a given frequecy
+		*  \param [in] _freq center frequency
+		*  \retval erb equivalent rectangular bandwidth (ERB) for the given center frequency
+		*   \eh Nothing is reported to the error handler.
+		*/
+		static float GetERBOfHumanAuditoryFilter(float _freq);
 
 
 
@@ -151,26 +151,26 @@ namespace Common {
 		////////////////////
 		// PRIVATE METHODS
 		///////////////////
-    static double CalculateAn(unsigned _order);
-    static double CalculateCn(unsigned _order);
-    static unsigned Factorial(unsigned x);
-  
+		static double CalculateAn(unsigned _order);
+		static double CalculateCn(unsigned _order);
+		static unsigned Factorial(unsigned x);
+	
 		////////////////
 		// ATTRIBUTES
 		////////////////
 		float generalGain;                                              // Gain applied to every sample obtained with Process
 
 		double samplingFreq;                                            // Keep the sampling rate at which audio samples were taken
-    unsigned order;                                                 // Keep the filter order
-    double b;                                                       // scale param of gamma distribution
-    double an;
-    double cn;
-    double f0;                                                      // center freq in Hz (also freq of impulse response tone)
-    double phase;                                                   // phase of the filter
-    float* prev_z_real;                                             // store previous samples between audio buffers
-    float* prev_z_imag;
-    float* prev_w_real;
-    float* prev_w_imag;
+		unsigned order;                                                 // Keep the filter order
+		double b;                                                       // scale param of gamma distribution
+		double an;
+		double cn;
+		double f0;                                                      // center freq in Hz (also freq of impulse response tone)
+		double phase;                                                   // phase of the filter
+		float* prev_z_real;                                             // store previous samples between audio buffers
+		float* prev_z_imag;
+		float* prev_w_real;
+		float* prev_w_imag;
 	};
 }
 #endif
